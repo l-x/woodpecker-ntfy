4bd8e4a1f8abafc5cfdc0c4a2140202c1164b4e7
Alexander Wühr <awuehr@pnyhf.eu>
2023-05-05 15:12:32 +0200

    Add tests for http request factory

--- main.go
+++ main.go
@@ -5,22 +5,102 @@ import (
 	"io"
 	"log"
 	"net/http"
+	"os"
+	"strings"
 )
 
-func main() {
+const defaultNtfyUrl = "https://ntfy.sh/woodpecker-ntfy"
+
+var (
+	server = setting{
+		valueFn:    env("PLUGIN_URL"),
+		fallbackFn: func() string { return defaultNtfyUrl }}
+	message = setting{valueFn: env("PLUGIN_MESSAGE")}
+	headers = map[string]setting{
+		"Authorization": {valueFn: getAuth},
+		"Title":         {valueFn: env("PLUGIN_TITLE")},
+		"Priority":      {valueFn: env("PLUGIN_PRIORITY")},
+		"Tags":          {valueFn: env("PLUGIN_TAGS")},
+		"Actions":       {valueFn: env("PLUGIN_ACTIONS")},
+		"Click":         {valueFn: env("PLUGIN_CLICK"), fallbackFn: env("CI_BUILD_LINK")},
+		"Icon":          {valueFn: env("PLUGIN_ICON"), fallbackFn: env("CI_COMMIT_AUTHOR_AVATAR")},
+	}
+)
+
+type setting struct {
+	valueFn    func() string
+	fallbackFn func() string
+}
+
+func (h *setting) getValue() string {
+	value := h.valueFn()
+
+	if value == "" && h.fallbackFn != nil {
+		return h.fallbackFn()
+	}
+
+	return value
+}
+
+func env(key string) func() string {
+	return func() string {
+		return os.Getenv(key)
+	}
+}
+
+func getAuth() string {
+	if token := os.Getenv("PLUGIN_TOKEN"); token != "" {
+		return fmt.Sprintf("Bearer %s", token)
+	}
+
+	return ""
+}
+
+func createRequest() (*http.Request, error) {
+	req, err := http.NewRequest(
+		"PUT",
+		server.getValue(),
+		strings.NewReader(message.getValue()),
+	)
+	if err != nil {
+		return req, err
+	}
+
+	for k, v := range headers {
+		req.Header.Set(k, v.getValue())
+	}
+
+	return req, nil
+}
+
+func notify() error {
 	req, err := createRequest()
-	checkErr(err)
+	if err != nil {
+		return err
+	}
 
 	res, err := http.DefaultClient.Do(req)
-	checkErr(err)
-
+	if err != nil {
+		return err
+	}
 	defer res.Body.Close()
 
 	b, err := io.ReadAll(res.Body)
-	checkErr(err)
-
-	if res.StatusCode != http.StatusOK {
-		checkErr(fmt.Errorf("%s %s", res.Status, b))
+	if err != nil {
+		return err
 	}
 
-	log.Printf("%s %s", res.Status, b)}
+	if res.StatusCode != http.StatusOK {
+		return fmt.Errorf("%s %s", res.Status, b)
+	}
+
+	log.Printf("%s %s", res.Status, b)
+
+	return nil
+}
+
+func main() {
+	if err := notify(); err != nil {
+		log.Fatal(err)
+	}
+}
