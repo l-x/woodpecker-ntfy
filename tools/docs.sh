ae6c31374a68ae9b757f0cfee9fcd9effa88c3ac
Alexander Wühr <alex@pnyhf.eu>
2023-05-22 13:03:58 +0200

    tune some docker related knobs

--- tools/docs.sh
+++ tools/docs.sh
@@ -7,7 +7,7 @@ description: plugin to send notifications to a ntfy.sh instance
 authors: l-x
 tags: [trigger, notify]
 containerImage: codeberg.org/l-x/woodpecker-ntfy
-containerImageUrl: https://codeberg.org/l-x/-/packages/container/woodpecker-ntfy
+containerImageUrl: https://codeberg.org/l-x/-/packages/container/woodpecker-ntfy/latest
 url: https://codeberg.org/l-x/woodpecker-ntfy
 ---
 
